22634faf914d9413eb2c0e24d9bb7b62263344d8
knockknockyoo <iheaven0129@gmail.com>
2023-07-31 10:37:20 -0400

    added help desk email address

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ let config = {
     emails_enabled: process.env.EMAILS_ENABLED ? process.env.EMAILS_ENABLED.toLowerCase() === 'true' : true,
     emails_url: process.env.EMAIL_URL ? process.env.EMAIL_URL : 'http://localhost:4010',
     // TODO official email needs to be verified
-    official_email: process.env.official ? process.env.OFFCIAL_EMAIL : 'help@crdc.org',
+    official_email: process.env.official ? process.env.OFFCIAL_EMAIL : 'CRDCHelpDesk@nih.gov',
     // Scheduled cronjob once a day (1am) eastern time at default
     schedule_job: process.env.SCHEDULE_JOB || "1 0 1 * * *"
 };
