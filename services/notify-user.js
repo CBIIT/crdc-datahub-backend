fe8ea626f0fb3abde8a3a7b0aed81a213faf7a4a
knockknockyoo <iheaven0129@gmail.com>
2023-08-03 15:58:42 -0400

    updated crdc submission committee

--- services/notify-user.js
+++ services/notify-user.js
@@ -20,16 +20,16 @@ class NotifyUser {
         console.error("Unable to load email constants from file, email not sent");
     }
 
-    async submitQuestionNotification(email, template_params, messageVariables) {
+    async submitQuestionNotification(messageVariables) {
         const message = replaceMessageVariables(this.email_constants.SUBMISSION_CONTENT, messageVariables);
         return await this.send(async () => {
             await this.emailService.sendNotification(
                 this.email_constants.NOTIFICATION_SENDER,
                 this.email_constants.SUBMISSION_SUBJECT,
                 await createEmailTemplate("notification-template.html", {
-                    message, ...template_params
+                    message, firstName: this.email_constants.APPLICATION_COMMITTEE_NAME
                 }),
-                email
+                this.email_constants.APPLICATION_COMMITTEE_EMAIL
             );
         });
     }
