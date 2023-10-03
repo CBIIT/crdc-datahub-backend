621662e8c4afe71f9bd899f10c730ef208ebeb18
Alec M <alec.mattu@nih.gov>
2023-10-03 13:51:26 -0400

    CRDCDH-454 listApprovedStudies and rename `dbGaPID` Changed `dbGaPID` to `dbGapID` to match schema

--- services/approved-studies.js
+++ services/approved-studies.js
@@ -1,5 +1,6 @@
 const {getCurrentTime} = require("../crdc-datahub-database-drivers/utility/time-utility");
 const ERROR = require("../constants/error-constants");
+const { verifySession } = require('../verifier/user-info-verifier');
 
 class ApprovedStudiesService {
 
@@ -15,14 +16,44 @@ class ApprovedStudiesService {
             console.error(ERROR.APPROVED_STUDIES_INSERTION);
         }
     }
+
+   /**
+     * List all approved studies in the collection. Supports filtering.
+     *
+     * @typedef {Object<string, any>} Filters K:V pairs of filters
+     * @param {Filters} [filters] Filters to apply to the query
+     * @returns {Promise<Object[]>} An array of ApprovedStudies
+     */
+    async listApprovedStudies(filters = {}) {
+        return await this.approvedStudiesCollection.aggregate([{ "$match": filters }]);
+    }
+
+   /**
+     * List Approved Studies API Interface.
+     *
+     * Note:
+     * - This is currently an open API for all logged-in users
+     *   filtering on Organization is not currently implemented.
+     *
+     * @api
+     * @param {Object} params Endpoint parameters
+     * @param {{ cookie: Object, userInfo: Object }} context request context
+     * @returns {Promise<Object[]>} An array of Organizations
+     */
+    async listApprovedStudiesAPI(params, context) {
+      verifySession(context)
+        .verifyInitialized();
+
+      return this.listApprovedStudies({});
+    }
 }
 
 class ApprovedStudies {
-    constructor(studyName, studyAbbreviation, dbGaPID, organizationName) {
+    constructor(studyName, studyAbbreviation, dbGapID, organizationName) {
         this.studyName = studyName;
         this.studyAbbreviation = studyAbbreviation;
-        if (dbGaPID) {
-            this.dbGaPID = dbGaPID;
+        if (dbGapID) {
+            this.dbGaPID = dbGapID;
         }
         // Optional
         if (organizationName) {
