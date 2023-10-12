8c561442157df0f7a9febf8271bd1f42bd0f3caf
knockknockyoo <iheaven0129@gmail.com>
2023-10-12 11:05:24 -0400

    added file upload status

--- domain/batch.js
+++ domain/batch.js
@@ -1,6 +1,6 @@
 const {getCurrentTime} = require("../crdc-datahub-database-drivers/utility/time-utility");
 const {v4} = require("uuid");
-const {BATCH} = require("../crdc-datahub-database-drivers/constants/batch-constants");
+const {BATCH, FILE} = require("../crdc-datahub-database-drivers/constants/batch-constants");
 class Batch {
     constructor(submissionID, bucketName, filePrefix, type, status, metadataIntention) {
         this._id = v4();
@@ -34,7 +34,7 @@ class BatchFile {
     constructor(fileName, size, signedURL, filePrefix) {
         this.fileName = fileName;
         this.size = size;
-        this.status = BATCH.STATUSES.NEW;
+        this.status = FILE.UPLOAD_STATUSES.NEW;
         if (signedURL) {
             this.signedURL = signedURL;
         }
