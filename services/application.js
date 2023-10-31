8f437666686c57df1365bf881642d41cf12f1bb3
Michael Tang <mtangmt@gmail.com>
2023-10-31 14:56:59 -0400

    remove unused methods

--- services/application.js
+++ services/application.js
@@ -433,15 +433,6 @@ class Application {
     }
 }
 
-
-function formatApplicantName(userInfo){
-    if (!userInfo) return "";
-    let firstName = userInfo?.firstName || "";
-    let lastName = userInfo?.lastName || "";
-    lastName = lastName.trim();
-    return firstName + (lastName.length > 0 ? " "+lastName : "");
-}
-
 function verifyReviewerPermission(context){
     verifySession(context)
         .verifyInitialized()
