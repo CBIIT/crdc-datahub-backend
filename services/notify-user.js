20df2e915186bd2932d74b80a0c628a7e141565b
knockknockyoo <iheaven0129@gmail.com>
2023-10-31 14:05:58 -0400

    fixed submitter batch

--- services/notify-user.js
+++ services/notify-user.js
@@ -170,12 +170,13 @@ class NotifyUser {
         });
     }
 
-    async withdrawSubmissionNotification(email, CCs, template_params, messageVariables) {
+    async withdrawSubmissionNotification(email, CCs, template_params, messageVariables, devTier) {
         const message = replaceMessageVariables(this.email_constants.WITHDRAW_DATA_SUBMISSION_CONTENT, messageVariables);
+        const subject = this.email_constants.WITHDRAW_DATA_SUBMISSION_SUBJECT;
         return await this.send(async () => {
             await this.emailService.sendNotification(
                 this.email_constants.NOTIFICATION_SENDER,
-                this.email_constants.WITHDRAW_DATA_SUBMISSION_SUBJECT,
+                isTierAdded(devTier) ? `${devTier} ${subject}` : subject,
                 await createEmailTemplate("notification-template.html", {
                     message, ...template_params
                 }),
@@ -200,6 +201,10 @@ class NotifyUser {
         });
     }
 }
+
+const isTierAdded = (devTier) => {
+    return devTier?.trim()?.length > 0
+};
 
 module.exports = {
     NotifyUser
