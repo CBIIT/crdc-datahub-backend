6fca5dc9a3ee111fd06d79d62d3859e8568f864f
Michael Tang <mtangmt@gmail.com>
2023-10-31 14:54:21 -0400

    add reject

--- services/notify-user.js
+++ services/notify-user.js
@@ -50,11 +50,11 @@ class NotifyUser {
     }
 
     async inquireQuestionNotification(email, template_params, messageVariables) {
-        const message = replaceMessageVariables(this.email_constants.REJECT_CONTENT, messageVariables);
+        const message = replaceMessageVariables(this.email_constants.INQUIRE_CONTENT, messageVariables);
         return await this.send(async () => {
             await this.emailService.sendNotification(
                 this.email_constants.NOTIFICATION_SENDER,
-                this.email_constants.REJECT_SUBJECT,
+                this.email_constants.INQUIRE_SUBJECT,
                 await createEmailTemplate("notification-template.html", {
                     message, ...template_params
                 }),
@@ -64,7 +64,7 @@ class NotifyUser {
         });
     }
 
-    async inquireQuestionNotification(email, template_params, messageVariables) {
+    async rejectQuestionNotification(email, template_params, messageVariables) {
         const message = replaceMessageVariables(this.email_constants.REJECT_CONTENT, messageVariables);
         return await this.send(async () => {
             await this.emailService.sendNotification(
