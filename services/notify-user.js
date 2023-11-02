b10c86255220ae1c9c7ca2e571abc06ea3277908
Wesley Lau <wesley.lau@nih.org>
2023-11-02 09:04:21 -0400

    Added Tier in Subject line

--- services/notify-user.js
+++ services/notify-user.js
@@ -144,12 +144,14 @@ class NotifyUser {
         });
     }
 
-    async submitDataSubmissionNotification(email, emailCCs,template_params, messageVariables) {
+    async submitDataSubmissionNotification(email, emailCCs,template_params, messageVariables, subjectVariables) {
         const message = replaceMessageVariables(this.email_constants.SUBMIT_DATA_SUBMISSION_CONTENT, messageVariables);
+        const subjectLine = replaceMessageVariables(this.email_constants.SUBMIT_DATA_SUBMISSION_SUBJECT, subjectVariables)
+
         return await this.send(async () => {
             await this.emailService.sendNotification(
                 this.email_constants.NOTIFICATION_SENDER,
-                this.email_constants.SUBMIT_DATA_SUBMISSION_SUBJECT,
+                subjectLine,
                 await createEmailTemplate("notification-template.html", {
                     message, ...template_params
                 }),
