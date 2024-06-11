31c252f1007294794bcef4d5c57316fda5a7608e
knockknockyoo <iheaven0129@gmail.com>
2024-06-11 12:48:43 -0400

    get dynamic model version from datacommon param

--- config.js
+++ config.js
@@ -83,7 +83,7 @@ function getModelUrl() {
     }
     const tier = extractTierName();
     // By default url
-    const modelUrl = ['https://raw.githubusercontent.com/CBIIT/crdc-datahub-models/', 'master', '/content.json']
+    const modelUrl = ['https://raw.githubusercontent.com/CBIIT/crdc-datahub-models/', tier || 'master', '/content.json']
     if (tier?.length > 0) {
         modelUrl[1] = tier.toLowerCase();
     }
