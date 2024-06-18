6b3b0899b824cba57e6bd9241f8244c0a933daf4
knockknockyoo <iheaven0129@gmail.com>
2024-06-18 09:03:24 -0400

    CTDC added

--- config.js
+++ config.js
@@ -47,7 +47,7 @@ let config = {
     model_url: getModelUrl(),
     //uploader configuration file template
     uploaderCLIConfigs: readUploaderCLIConfigTemplate(),
-    dataCommonsList: process.env.DATA_COMMONS_LIST ? process.env.DATA_COMMONS_LIST : ["CDS", "ICDC"]
+    dataCommonsList: process.env.DATA_COMMONS_LIST ? process.env.DATA_COMMONS_LIST : ["CDS", "ICDC", "CTDC"]
 };
 config.mongo_db_connection_string = `mongodb://${config.mongo_db_user}:${config.mongo_db_password}@${config.mongo_db_host}:${process.env.MONGO_DB_PORT}`;
 
