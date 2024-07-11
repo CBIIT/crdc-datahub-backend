dd7062ebf24072454a8b26762e824e7409d8ee10
knockknockyoo <iheaven0129@gmail.com>
2024-07-11 17:33:24 -0400

    changed constant name

--- services/batch-service.js
+++ services/batch-service.js
@@ -8,7 +8,7 @@ const {SUBMISSIONS_COLLECTION} = require("../crdc-datahub-database-drivers/datab
 const {getCurrentTime} = require("../crdc-datahub-database-drivers/utility/time-utility");
 const LOAD_METADATA = "Load Metadata";
 const OMIT_DCF_PREFIX = 'omit-DCF-prefix';
-const DG_4DFC = "dg.4DFC";
+const DCF_PREFIX = "dg.4DFC";
 class BatchService {
     constructor(s3Service, batchCollection, sqsLoaderQueue, awsService, prodURL, dataModelInfo) {
         this.s3Service = s3Service;
@@ -32,7 +32,7 @@ class BatchService {
             }));
         } else {
             // The prefix "dg.4DFC" only need to be added if "omit-dcf-prefix" is null or set to false in the data model
-            const dataModelPrefix = (this.dataModelInfo?.[aSubmission?.dataCommons]?.[OMIT_DCF_PREFIX]) ? "" : DG_4DFC;
+            const dataModelPrefix = (this.dataModelInfo?.[aSubmission?.dataCommons]?.[OMIT_DCF_PREFIX]) ? "" : DCF_PREFIX;
             params.files.forEach((file) => {
                 if (file.fileName) {
                     newBatch.addDataFile(file.fileName, file.size, this.prodURL, aSubmission?.dataCommons, aSubmission?.studyID, dataModelPrefix);
