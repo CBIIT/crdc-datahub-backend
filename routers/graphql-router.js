15abf61c040d13676c3a50f8f838b5c0e2f300c9
knockknockyoo <iheaven0129@gmail.com>
2024-10-15 16:59:24 -0400

    fed monitor change

--- routers/graphql-router.js
+++ routers/graphql-router.js
@@ -52,7 +52,7 @@ dbConnector.connect().then(async () => {
     const organizationService = new Organization(organizationCollection, userCollection, submissionCollection, applicationCollection, approvedStudiesCollection);
     const approvedStudiesService = new ApprovedStudiesService(approvedStudiesCollection, organizationService);
 
-    const userService = new User(userCollection, logCollection, organizationCollection, notificationsService, submissionCollection, applicationCollection, config.official_email, config.submission_helpdesk, config.emails_url, config.tier, organizationService);
+    const userService = new User(userCollection, logCollection, organizationCollection, notificationsService, submissionCollection, applicationCollection, config.official_email, config.emails_url, config.tier, organizationService);
     const s3Service = new S3Service();
     const batchCollection = new MongoDBCollection(dbConnector.client, DATABASE_NAME, BATCH_COLLECTION);
     const awsService = new AWSService(submissionCollection, userService);
