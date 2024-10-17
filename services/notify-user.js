278e7c1f28d3ab819ee9c4514075f92fa59f173e
knockknockyoo <iheaven0129@gmail.com>
2024-10-17 09:27:36 -0400

    remove space

--- services/notify-user.js
+++ services/notify-user.js
@@ -262,9 +262,6 @@ class NotifyUser {
                 CCs
             );
         });
-
-
-
     }
 
     async inactiveSubmissionNotification(email, CCs, template_params, messageVariables, tier) {
