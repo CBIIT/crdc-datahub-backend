cd1545c71ab4dcbdc3545d11c25265157ca3427c
vshand11 <105606628+vshand11@users.noreply.github.com>
2025-01-22 10:12:08 -0500

    merge changes in submission.js

--- services/submission.js
+++ services/submission.js
@@ -1281,6 +1281,39 @@ class Submission {
         return await this.userService.getCollaboratorsByStudyID(aSubmission.studyID, aSubmission.submitterID);
     }
 
+    /**
+     * API: get releases data
+     * @param {*} params 
+     * @param {*} context 
+     * @returns 
+     */
+    async getReleasedNodeByIDs(params, context)
+    {
+        verifySession(context)
+            .verifyInitialized()
+            .verifyPermission(DS_PERMISSION.VIEW);
+        const {
+            submissionID = submissionID,
+            nodeType = nodeType,
+            nodeID = nodeID,
+            status = status
+        } = params; // all three parameters are required in GraphQL API
+        const results = await this.dataRecordService.getReleasedNode(submissionID, nodeType, nodeID, status);
+        if(results && results.length > 0) 
+        {
+            const result = results[0];
+            if(result?.props)
+            {
+                result.props = JSON.stringify(result.props);
+            }
+            return result;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
     async verifySubmitter(submissionID, userInfo) {
         const aSubmission = await findByID(this.submissionCollection, submissionID);
         if (!aSubmission) {
