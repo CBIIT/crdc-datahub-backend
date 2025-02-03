463ea7c420331ce07cb3c8b51a2f9fdafbe13470
knockknockyoo <iheaven0129@gmail.com>
2025-02-03 14:35:09 -0500

    log

--- services/submission.js
+++ services/submission.js
@@ -2112,15 +2112,7 @@ function logDaysDifference(inactiveDays, accessedAt, submissionID) {
 }
 
 function getFormattedDateTime(date) {
-    return new Intl.DateTimeFormat('en-US', {
-        timeZone: 'America/New_York',
-        year: 'numeric',
-        month: '2-digit',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    }).format(date).replace(',', '').replace(/\//g, ':');
+    return date?.toLocaleString("en-US", { timeZone: "America/New_York" });
 }
 
 
