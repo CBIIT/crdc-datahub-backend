e17f9fe4c000298304df250a8c492c2e63ea4a51
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2025-05-08 14:24:53 -0400

    update checkForReadOnlyViolation tests

--- test/graphql-queries/edit-organization.test.js
+++ test/graphql-queries/edit-organization.test.js
@@ -1,37 +1,64 @@
-const { ERROR } = require("../../crdc-datahub-database-drivers/constants/error-constants");
 const {Organization} = require("../../crdc-datahub-database-drivers/services/organization");
 
 describe('Test Organization Service', () => {
-
     let organization;
 
     beforeAll(() => {
-        const mockOrganizationCollection = {
-            aggregate: jest.fn().mockResolvedValue([{
-                "_id": "437e864a-621b-40f5-b214-3dc368137081",
-                "name": "NA",
-                "abbreviation": "NA",
-                "status": "Active",
-                "bucketName": "crdc-hub-dev-submission",
-                "rootPath": "437e864a-621b-40f5-b214-3dc368137081",
-                "createdAt": {
-                    "$date": "2025-05-06T00:00:00.000Z"
-                },
-                "updateAt": {
-                    "$date": "2025-05-06T00:00:00.000Z"
-                },
-                "studies": [],
-                "readOnly": true
-            }])
-        }
-        organization = new Organization(mockOrganizationCollection);
+        organization = new Organization();
     })
 
-    test('Get error editing NA program', async () => {
-        await expect(organization.editOrganization("437e864a-621b-40f5-b214-3dc368137081", null))
-            .rejects.toThrow(ERROR.CANNOT_UPDATE_READ_ONLY_PROGRAM);
+    test('Check for read-only violations', async () => {
+        let initialNaProgram = {
+            "_id": "437e864a-621b-40f5-b214-3dc368137081",
+            "name": "NA",
+            "abbreviation": "NA",
+            "description": "This is a catch-all place for all studies without a program associated.",
+            "status": "Active",
+            "bucketName": "crdc-hub-dev-submission",
+            "rootPath": "437e864a-621b-40f5-b214-3dc368137081",
+            "createdAt": {
+                "$date": "2025-05-06T00:00:00.000Z"
+            },
+            "updateAt": {
+                "$date": "2025-05-06T00:00:00.000Z"
+            },
+            "studies": [],
+            "readOnly": true
+        };
+        const defaultParams = {
+            "name": "NA",
+            "abbreviation": "NA",
+            "description": "This is a catch-all place for all studies without a program associated.",
+            "status": "Active",
+            "studies": ["test"],
+        };
+        let params = {...defaultParams};
+        // read only flag with no changes
+        expect(organization.checkForReadOnlyViolation(initialNaProgram, params)).toBe(false);
+        // read only flag with name update violation
+        params.name = "test";
+        expect(organization.checkForReadOnlyViolation(initialNaProgram, params)).toBe(true);
+        params = {...defaultParams};
+        // read only flag with abbreviation update violation
+        params.abbreviation = "test";
+        expect(organization.checkForReadOnlyViolation(initialNaProgram, params)).toBe(true);
+        params = {...defaultParams};
+        // read only flag with description update violation
+        params.description = "test";
+        expect(organization.checkForReadOnlyViolation(initialNaProgram, params)).toBe(true);
+        params = {...defaultParams};
+        // read only flag with status update violation
+        params.status = "test";
+        expect(organization.checkForReadOnlyViolation(initialNaProgram, params)).toBe(true);
+        params = {...defaultParams};
+        // read only flag with no updates
+        params = {};
+        expect(organization.checkForReadOnlyViolation(initialNaProgram, params)).toBe(false);
+        params = {...defaultParams};
+        // no read only flag
+        initialNaProgram.readOnly = false;
+        params.name = "test";
+        expect(organization.checkForReadOnlyViolation(initialNaProgram, params)).toBe(false);
     });
-
-
 
 });
