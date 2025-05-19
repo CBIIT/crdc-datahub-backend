80b29b78bca156a7366169693dec7d2445573cca
knockknockyoo <iheaven0129@gmail.com>
2025-05-19 16:35:20 -0400

    no optional chain.

--- services/authorization-service.js
+++ services/authorization-service.js
@@ -131,7 +131,7 @@ class AuthorizationService {
             }
             const { permission, scopes: inputScope, scopeValues: inputScopeValues } = parsePermissionString(p);
             const outputScopes = await this.#getScopePermission(user, {scopes: inputScope, scopeValues: inputScopeValues}, permission);
-            const hasAnyScope = outputScopes?.some(scope => this.#EVERY_SCOPE_VALUES?.includes(scope.scope));
+            const hasAnyScope = outputScopes?.some(scope => this.#EVERY_SCOPE_VALUES.includes(scope.scope));
             if (this.#allPermissionNamesSet.has(permission) && (hasAnyScope)) {
                 filtered.push(p);
             }
