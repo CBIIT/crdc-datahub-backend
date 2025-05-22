c1fb6100514fec933ff7d4948bfd3fba34ba6bd8
vshand11 <105606628+vshand11@users.noreply.github.com>
2025-05-22 10:21:53 -0400

    set the max request siz to 10 MB

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const accessLogStream = fs.createWriteStream(join(__dirname, LOGS_FOLDER, 'acces
 const app = express();
 app.use(cors());
 app.use(logger('combined', { stream: accessLogStream }))
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, 'public')));
 
