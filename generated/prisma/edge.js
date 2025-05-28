447f1afd4324ddf3cd94f69c4dd10b8427517da1
Ming Ying <yingming18@gmail.com>
2025-05-28 13:44:27 -0400

    Modified getApprovedStudy to partially use Prisma

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -461,7 +461,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
--- generated/prisma/index.js
+++ generated/prisma/index.js
@@ -462,7 +462,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
