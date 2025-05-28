daa6b2f5856c3c8883602f392cab1f254b9042ea
Ming Ying <yingming18@gmail.com>
2025-05-28 14:44:19 -0400

    Better handle study not found situation

--- dao/approvedStudy.js
+++ dao/approvedStudy.js
@@ -3,6 +3,9 @@ const prisma = require("../prisma");
 async function getApprovedStudyByID(id) {
     const study = await prisma.approvedStudies.findUnique({where: {id: id}})
     //prisma doesn't allow using _id, so we have to map it
+    if (!study) {
+        return null
+    }
     return {...study, _id: study.id}
 }
 
