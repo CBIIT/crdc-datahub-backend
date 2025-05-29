61ac4541948f880bb661c5f5735ab5a4b63fce83
Austin Mueller <64418315+AustinSMueller@users.noreply.github.com>
2025-05-29 16:02:04 -0400

    Revert "retry failed S3 requests"
    This reverts commit 6ee88344cb8e7a8e04bb5d82d126fb912f82cb43.

--- services/s3-service.js
+++ services/s3-service.js
@@ -211,25 +211,9 @@ class S3Service {
         };
         let fileObjects = [];
         let isTruncated = true;
-        let maxAttempts = 5;
-        let attempts = 0;
         try{
             while (isTruncated){
-                attempts++;
-                let data;
-                try{
-                    data = await this.#listObjectsV2(listParams);
-                }
-                catch (err){
-                    if (attempts <= maxAttempts){
-                        console.error(`retrying S3 request after error: ${err.toString()}`);
-                        let sleepTime = 100*2**(attempts-1);
-                        console.error(`waiting ${sleepTime} ms before retrying`)
-                        await new Promise(r => setTimeout(r, sleepTime));
-                        continue;
-                    }
-                    throw err;
-                }
+                const data = await this.#listObjectsV2(listParams);
                 if (data.Contents) {
                     fileObjects.push(...data.Contents);
                 }
