84077203afe745fadc21b186156aef9c0ca0dba3
knockknockyoo <iheaven0129@gmail.com>
2025-06-12 11:37:37 -0400

    revert back.

--- generated/prisma/edge.js
+++ generated/prisma/edge.js
@@ -35,12 +35,12 @@ exports.Prisma = Prisma
 exports.$Enums = {}
 
 /**
- * Prisma Client JS version: 6.8.2
- * Query Engine version: 2060c79ba17c6bb9f5823312b6f6b7f4a845738e
+ * Prisma Client JS version: 6.9.0
+ * Query Engine version: 81e4af48011447c3cc503a190e86995b66d2a28e
  */
 Prisma.prismaVersion = {
-  client: "6.8.2",
-  engine: "2060c79ba17c6bb9f5823312b6f6b7f4a845738e"
+  client: "6.9.0",
+  engine: "81e4af48011447c3cc503a190e86995b66d2a28e"
 }
 
 Prisma.PrismaClientKnownRequestError = PrismaClientKnownRequestError;
@@ -436,7 +436,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "/Users/yingm3/work/code/data-hub/crdc-datahub-backend/generated/prisma",
+      "value": "/Users/yoos4/WebstormProjects/new-crdc-datahub-backend/generated/prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -450,7 +450,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/yingm3/work/code/data-hub/crdc-datahub-backend/prisma/schema.prisma",
+    "sourceFilePath": "/Users/yoos4/WebstormProjects/new-crdc-datahub-backend/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
@@ -458,8 +458,8 @@ const config = {
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
-  "clientVersion": "6.8.2",
-  "engineVersion": "2060c79ba17c6bb9f5823312b6f6b7f4a845738e",
+  "clientVersion": "6.9.0",
+  "engineVersion": "81e4af48011447c3cc503a190e86995b66d2a28e",
   "datasourceNames": [
     "db"
   ],
