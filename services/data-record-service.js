d330db92638d6e70a0de06c91dcbf24e4db9eae7
knockknockyoo <iheaven0129@gmail.com>
2025-06-18 14:05:54 -0400

    Fixed merge conflict.

--- services/data-record-service.js
+++ services/data-record-service.js
@@ -672,7 +672,6 @@ class DataRecordService {
         if(!newNode){
             throw new Error(ERRORS.INVALID_NODE_NOT_FOUND);
         }
-        this.#processParents(newNode)
         newNode.props = JSON.stringify(newNode.props);
 
         // get release node
@@ -691,21 +690,8 @@ class DataRecordService {
         }
         const releaseNode = results[0];
         releaseNode.status = status;
-        this.#processParents(releaseNode)
         releaseNode.props = JSON.stringify(releaseNode.props);
         return [newNode, releaseNode]
-    }
-
-    /**
-     * #processParents
-     * @param {*} node 
-     */
-    #processParents(node){
-        if (node.parents && node.parents.length > 0) {
-            node.parents.forEach((parent) => {
-                node.props[`${parent.parentType}.${parent.parentIDPropName}`] = parent.parentIDValue;
-            });
-        }
     }
 }
 
