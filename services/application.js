d7b484891cd78951b438b0593a8cbcdad48b4d53
knockknockyoo <iheaven0129@gmail.com>
2025-07-09 10:19:54 -0400

    Some data clean up logic Applied the generic DAO.

--- services/application.js
+++ services/application.js
@@ -145,10 +145,6 @@ class Application {
                 applicantName: formatName(userInfo),
                 applicantEmail: userInfo.email
             },
-            organization: {
-                _id: userInfo?.organization?.orgID || userInfo?.organization?._id ,
-                name: userInfo?.organization?.orgName || userInfo?.organization?.name
-            },
             history: [HistoryEventBuilder.createEvent(userInfo._id, NEW, null)],
             createdAt: timestamp,
             updatedAt: timestamp,
@@ -156,6 +152,14 @@ class Application {
             programDescription: application?.programDescription,
             version: (application?.version)? application.version : await this._getApplicationVersionByStatus(NEW)
         };
+
+        if (userInfo?.organization?.orgID) {
+            newApplicationProperties.organization = {
+                _id: userInfo?.organization?.orgID,
+                name: userInfo?.organization?.orgName || ""
+            }
+        }
+
         application = {
             ...application,
             ...newApplicationProperties
