82f40442a85ea75ef42ae18be6cba4ca6218c760
wfy1997 <60631776+wfy1997@users.noreply.github.com>
2025-07-10 09:33:06 -0400

    update isApplicationEmpty async function to isApplicationEmpty function

--- services/application.js
+++ services/application.js
@@ -420,7 +420,7 @@ class Application {
         let updated = null
         let deleteApplication = false;
         const utilityService = new UtilityService();
-        if (await utilityService.isApplicationDeletion(aApplication)) {
+        if (await utilityService.isApplicationEmpty(aApplication)) {
             updated = await this.dbService.deleteOne(APPLICATION, {_id: document._id});
             deleteApplication = true;
         } else{
