050c7b1f88c802d98bb4d305894b81d0eafa02ec
knockknockyoo <iheaven0129@gmail.com>
2025-07-11 12:58:04 -0400

    removed TODO. Fixed trailing.

--- test/services/application.test.js
+++ test/services/application.test.js
@@ -249,7 +249,6 @@ describe('Application', () => {
         });
     });
 
-    // TODO
     describe('saveApplication', () => {
         it('creates new application if no id', async () => {
             userScopeMock.isNoneScope.mockReturnValue(false);
