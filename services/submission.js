97aab650120264f4e65dff122d958053a08a2bf4
knockknockyoo <iheaven0129@gmail.com>
2025-09-02 16:40:16 -0400

    Fixed the user organization. fixed the collaborator's organization.

--- services/submission.js
+++ services/submission.js
@@ -1194,11 +1194,21 @@ class Submission {
                     throw new Error(ERROR.INVALID_COLLABORATOR_PERMISSION);
                 }
             }
-            collaborator.collaboratorName = user.lastName + ", " + user.firstName ;
-            collaborator.Organization = user.organization;
+            collaborator.collaboratorName = user.lastName + ", " + user.firstName;
+            if (user?.organization && Object.keys(user?.organization)?.length > 0) {
+                collaborator.Organization = {
+                    id: user.organization.id,
+                    orgName: user.organization.name
+                };
+            }
         }
         const result = await this.submissionDAO.update(aSubmission?._id, this._prepareUpdateData({collaborators}));
         if (result) {
+            (result?.collaborators || []).forEach(c => {
+                if (c.Organization?.id) {
+                    c.Organization.orgID = c.Organization?.id;
+                }
+            })
             return getDataCommonsDisplayNamesForSubmission(result);
         }
         throw new Error(ERROR.FAILED_ADD_SUBMISSION_COLLABORATOR);
