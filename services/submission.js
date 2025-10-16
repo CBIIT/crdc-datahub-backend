cc3cd85bf567e2e31f95c2a7b663bd6a41d3466a
knockknockyoo <iheaven0129@gmail.com>
2025-10-16 13:32:36 -0400

    1. added test codes 2. created centralized the private _hasStudyAccess

--- services/submission.js
+++ services/submission.js
@@ -1410,7 +1410,7 @@ class Submission {
                 return users?.filter(({ role, studies }) => (
                     role === USER.ROLES.ADMIN ||
                     (role === USER.ROLES.FEDERAL_LEAD &&
-                        (isAllStudy(studies) || validateStudyAccess(studies, approvedStudy?.id)))
+                        (this._hasStudyAccess(studies, approvedStudy?.id)))
                 ));
             })(),
         ]);
@@ -2512,6 +2512,10 @@ class Submission {
             // Don't throw error for logging failures as it shouldn't break the main flow
             return null;
         }
+    }
+
+    _hasStudyAccess(userStudies, studyID) {
+        return isAllStudy(userStudies) || validateStudyAccess(userStudies, studyID);
     }
 }
 
